e3cc3d67882201a8f23531de7ebc5bf4bf48f603
diya2006git <ds0673745@gmail.com>
2026-03-02 13:44:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Customer-Feedback-Monitor/',
   plugins: [react()],
-});
+
+  build: {
+    outDir: 'dist'
+  },
+
+  resolve: {
+    alias: {
+      '@': '/'
+    }
+  }
+})
